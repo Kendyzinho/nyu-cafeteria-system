9d12bc80ba29e7f89cb877d7bcbafe8ef70b477f
Bairon Núñez <lensko_2002@hotmail.com>
2026-05-22 03:59:38 -0400

    Implementación tokens
    Se usa jwt y bscrypt para la implementación de tokens

--- backend/src/controllers/controllers.module.ts
+++ backend/src/controllers/controllers.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MenuController } from './menu/menu.controller';
 import { PedidosController } from './pedidos/pedidos.controller';
 import { PlanesComidaController } from './planes-comida/planes-comida.controller';
@@ -12,6 +15,7 @@ import { PedidoEntity } from 'src/database/entities/pedido.entity';
 import { PlanesCatalogoEntity } from 'src/database/entities/planes-catalogo.entity';
 import { PromocionEntity } from 'src/database/entities/promocion.entity';
 import { MockUsuarioEntity } from 'src/database/entities/mock-usuario.entity';
+import { DetallePedidoEntity } from 'src/database/entities/detalle-pedido.entity';
 import { MenuService } from 'src/providers/menu/menu.service';
 import { PedidosService } from 'src/providers/pedidos/pedidos.service';
 import { PlanesComidaService } from 'src/providers/planes-comida/planes-comida.service';
@@ -19,12 +23,23 @@ import { StockService } from 'src/providers/stock/stock.service';
 import { PromocionesService } from 'src/providers/promociones/promociones.service';
 import { UsuariosService } from 'src/providers/usuarios/usuarios.service';
 import { AuthService } from 'src/providers/auth/auth.service';
+import { JwtStrategy } from 'src/common/strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+      inject: [ConfigService],
+    }),
     TypeOrmModule.forFeature([
       ComidaEntity,
       PedidoEntity,
+      DetallePedidoEntity,
       PlanesCatalogoEntity,
       PromocionEntity,
       MockUsuarioEntity,
@@ -47,6 +62,7 @@ import { AuthService } from 'src/providers/auth/auth.service';
     PromocionesService,
     UsuariosService,
     AuthService,
+    JwtStrategy,
   ],
 })
 export class ControllersModule {}
