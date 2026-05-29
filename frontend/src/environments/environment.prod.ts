63772222fc59fb005549dcf2011a18981521aae4
Kendyzinho <rafakendy2003@gmail.com>
2026-05-29 19:35:16 -0400

    Apuntando al Render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBackendUrl: 'https://tu-backend-en-render.com/api', 
+  apiBackendUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
   apiPagosUrl: 'https://api-mock-equipo5.com/pagos', 
   apiResidenciaUrl: 'https://api-mock-equipo2.com/residencia', 
   apiMatriculaUrl: 'https://api-mock-equipo1.com/matricula', 
