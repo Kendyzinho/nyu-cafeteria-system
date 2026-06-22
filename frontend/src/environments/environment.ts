fed48b6f7fc2028aa74e30b2264e9c0a2c4216d8
Kendyzinho <rafakendy2003@gmail.com>
2026-06-22 00:15:28 -0400

    Cambio menores

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBackendUrl: 'https://nyu-cafeteria-api.onrender.com/api',
-  apiPagosUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
+  apiPagosUrl: 'https://nyu-cafeteria-api.onrender.com/api/pagos', 
   apiResidenciaUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
   apiMatriculaUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
   apiBibliotecaUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
