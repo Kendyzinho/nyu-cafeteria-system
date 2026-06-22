cefc1277d0365c74cce0da74b64149cdf40903f6
Kendyzinho <rafakendy2003@gmail.com>
2026-06-21 23:54:32 -0400

    Eliminando las URLs mocks

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiBackendUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
-  apiPagosUrl: 'https://api-mock-equipo5.com/pagos', 
-  apiResidenciaUrl: 'https://api-mock-equipo2.com/residencia', 
-  apiMatriculaUrl: 'https://api-mock-equipo1.com/matricula', 
-  apiBibliotecaUrl: 'https://api-mock-equipo3.com/biblioteca', 
+  apiPagosUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
+  apiResidenciaUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
+  apiMatriculaUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
+  apiBibliotecaUrl: 'https://nyu-cafeteria-api.onrender.com/api', 
 };
